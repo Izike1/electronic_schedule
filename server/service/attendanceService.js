c7abd7265041fd5549ab36d3673642fa57a402a4
Izike1 <maxim.larionenko@gmail.com>
2023-12-10 01:26:16 +0300

    attendanceController update

--- server/service/attendanceService.js
+++ server/service/attendanceService.js
@@ -1,4 +1,31 @@
+const {Attendance, Schedule} = require('../models/models')
+const sequelize = require('../db')
+const {writeToLogFile} = require("../logger");
+const ApiError = require("../error/ApiError");
+
 class AttendanceService {
+    async updateAttendance(attendanceData) {
+        await sequelize.transaction(async (t) => {
+            for (const attendanceItem of attendanceData) {
+                const scheduleId = attendanceItem.scheduleId;
+                const attendanceDetails = attendanceItem.attendanceDetails;
+
+                const schedule = await Schedule.findByPk(scheduleId);
+
+                if (schedule) {
+                    const attendanceEntries = attendanceDetails.map((detail) => ({
+                        ScheduleId: schedule.id,
+                        userId: detail.userId,
+                        isPresent: detail.isPresent,
+                    }));
+                    await Attendance.bulkCreate(attendanceEntries, {transaction: t});
+                } else {
+                    console.error(`Schedule with ID ${scheduleId} not found.`);
+                }
+            }
+        });
+    }
 
 }
+
 module.exports = new AttendanceService();
